4053e18ede8ac3c612797ab68852a5c8ed8b4b21
Robin Wohlers-Reichel <robin.wohlersreichel@gmail.com>
2020-07-27 02:28:34 +1000

    progress

--- bootstrap/payload/runme.sh
+++ bootstrap/payload/runme.sh
@@ -2,6 +2,80 @@
 
 set -exuo pipefail
 
-echo "hello from the runme"
-echo $(id -u)
-echo "end"
+SD_CARD=/dev/mmcblk0
+
+# check if sd card is present
+ls ${SD_CARD}
+
+# nuke sd card partition table
+dd if=/dev/zero of=${SD_CARD} bs=512 count=100
+
+# reload partition table
+partprobe ${SD_CARD}
+
+# make a partition table
+fdisk /dev/mmcblk0 <<EOF
+o
+w
+EOF
+
+# make a 50M boot partition of type W95 FAT32 (LBA)
+fdisk /dev/mmcblk0 <<EOF
+n
+p
+1
+
++100M
+t
+c
+w
+EOF
+
+sleep 5
+
+# make a root partition of type linux
+fdisk /dev/mmcblk0 <<EOF
+n
+p
+2
+
+
+t
+2
+83
+w
+EOF
+
+echo "formatting"
+
+# format partitions
+mkfs.fat -F 32 -n BOOT /dev/mmcblk0p1
+mkfs.ext4 -F -L rootfs -O ^64bit,^huge_file,^metadata_csum /dev/mmcblk0p2
+
+# mount new partitions
+mkdir /tmp/boot
+mkdir /tmp/root
+mount /dev/mmcblk0p1 /tmp/boot
+mount /dev/mmcblk0p2 /tmp/root
+
+echo "untaring"
+
+# untar
+tar -C /tmp/boot -xJf /boot.tar.xz
+rsync -ah --info=progress2 /copyme/ /tmp/root/
+
+echo "patching"
+
+# patch partition UUID in fstab and boot args
+BOOT_UUID=$(blkid /dev/mmcblk0 -s PTUUID -o value)
+sed -i "s/56cd6262/${BOOT_UUID}/g" /tmp/root/etc/fstab
+sed -i "s/56cd6262/${BOOT_UUID}/g" /tmp/boot/cmdline.txt
+touch /tmp/boot/ssh
+
+sync
+
+echo "rebooting"
+
+sleep 5
+
+reboot
